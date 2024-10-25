be114241e372635b90738236c4dc35e5795359c9
Bladen777 <braden.a.j.kowalchuk@live.ca>
2024-10-25 15:07:14 -0600

    finish off module_01

---
+++ module_01_exercise/build/module01.js
@@ -0,0 +1,8 @@
+"use strict";
+let x;
+let y = 1;
+let z;
+function addNumbers(x, y) {
+    return x + y;
+}
+console.log(addNumbers(3, 6));
--- module_01_exercise/module01.ts
+++ module_01_exercise/module01.ts
@@ -1,3 +1,7 @@
+let x:number;
+let y=1;
+let z;
+
 function addNumbers(x:number, y:number) {
     return x + y;
   }
